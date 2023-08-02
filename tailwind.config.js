acaa5356e610abf4f7dbe22289cb5b2dda867f13
Yash Kamble <112558970+yash-devop@users.noreply.github.com>
2023-08-02 21:14:29 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        Netflix: ["Netflix", "san-serif"],
+      },
+  },
+
+  },
+  plugins: [],
+}
